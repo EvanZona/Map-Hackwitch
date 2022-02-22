7fe16160a465889602f8be6a555de57191cbe621
Evan Zona <evanbzona@gmail.com>
2022-02-22 10:37:21 -0600

    Setup initial Map

--- Map Hackwitch/ContentView.swift
+++ Map Hackwitch/ContentView.swift
@@ -6,11 +6,19 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 42.15704,
+            longitude: -88.14812),//coords of the HS
+        span: MKCoordinateSpan(// how zoomed in
+            latitudeDelta: 0.05, // amount zoomed in are we?
+            longitudeDelta: 0.05)
+    )
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $region)
     }
 }
 
