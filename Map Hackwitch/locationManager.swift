a9edbc3ced2bf20d4b109219cd77dd809f4fd355
Evan Zona <evanbzona@gmail.com>
2022-02-22 10:58:46 -0600

    Added user location

---
+++ Map Hackwitch/locationManager.swift
@@ -0,0 +1,20 @@
+//
+//  locationManager.swift
+//  Map Hackwitch
+//
+//  Created by Student on 2/22/22.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+   
+    override init() {
+            super.init()
+            locationManager.delegate = self
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+}
